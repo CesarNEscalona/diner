1f2c89e54ebfd09e704a76d0a4560896eb112f83
Cesar Escalona <cesarescalona10@gmail.com>
2021-06-01 14:15:12 -0400

    Adding updated files

--- model/data-layer.php
+++ model/data-layer.php
@@ -1,24 +1,94 @@
 <?php
 
 /* data-layer.php
-    return data for the diner app
+ * Return data for the diner app
+ *
+ */
 
-*/
+//Require the config file
+require_once($_SERVER['DOCUMENT_ROOT'].'/../config.php');
 
-// Get the meals for the order form
-function getMeals()
+class DataLayer
 {
-    return array("breakfast", "brunch", "lunch", "dinner", "dessert");
-}
+    // Add a field for the database object
+    private $_dbh;
 
-// Return true if meal is valid
-function validMeal($meal)
-{
-    return in_array($meal, getMeals());
-}
+    // Define a constructor
+    function __construct()
+    {
+        //Connect to the database
+        try {
+            //Instantiate a PDO database object
+            $this->_dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+            //echo "Connected to database!";
+        }
+        catch(PDOException $e) {
+            //echo $e->getMessage();  //for debugging only
+            die ("Oh darn! We're having a bad day. Please call to place your order.");
+        }
+    }
 
-// Get the conds for the order form
-function getConds()
-{
-    return array("ketchup", "mustard", "sriracha");
+    // Saves an order to the database
+
+    /**
+     * saveOrder accepts an Order object
+     * @param $order
+     * @return string
+     */
+    function saveOrder($order)
+    {
+        //1. Define the query
+        $sql = "INSERT INTO orders (food, meal, condiments) 
+                VALUES (:food, :meal, :condiments)";
+
+        //2. Prepare the statement
+        $statement = $this->_dbh->prepare($sql);
+
+        //3. Bind the parameters
+        $statement->bindParam(':food', $order->getFood(), PDO::PARAM_STR);
+        $statement->bindParam(':meal', $order->getMeal(), PDO::PARAM_STR);
+        $statement->bindParam(':condiments', $order->getCondiments(), PDO::PARAM_STR);
+
+        //4. Execute the query
+        $statement->execute();
+
+        //5. Process the results (get OrderID)
+        $id = $this->_dbh->lastInsertId();
+        return $id;
+    }
+
+    function getOrders()
+    {
+        //1. Define the query
+        $sql = "SELECT order_id, food, meal, condiments, order_date FROM orders";
+
+        //2. Prepare the statement
+        $statement = $this->_dbh->prepare($sql);
+
+        //3. Bind the parameters
+        //$statement->bindParam(':food', $order->getFood(), PDO::PARAM_STR);
+        //$statement->bindParam(':meal', $order->getMeal(), PDO::PARAM_STR);
+        //$statement->bindParam(':condiments', $order->getCondiments(), PDO::PARAM_STR);
+
+        // No parameters for this function....
+
+        //4. Execute the query
+        $statement->execute();
+
+        //5. Process the results (get OrderID)
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
+
+    // Get the meals for the order form
+    static function getMeals()
+    {
+        return array("breakfast", "brunch", "lunch", "dinner");
+    }
+
+    // Get the condiments for the order 2 form
+    static function getCondiments()
+    {
+        return array("ketchup", "mustard", "mayo", "sriracha");
+    }
 }
--- model/validation.php
+++ model/validation.php
@@ -1,25 +1,37 @@
 <?php
 
 /* validation.php
-    Validate data for the diner app
-*/
+ * Validate data for the diner app
+ *
+ */
 
-// Returns true if a food is valid
-function validFood($food)
+class Validation
 {
-    return strlen(trim($food)) >= 2;
-}
-
-// Returns true if a cond is valid
-function validCondiments($condiments)
-{
-    $validCondiments = getConds();
-    // Make sure each selected condiment is valid
-    foreach ($condiments as $userChoice) {
-        if(!in_array($userChoice, $validCondiments)){
-            return false;
-        }
+    //Return true if a food is valid
+    static function validFood($food)
+    {
+        return strlen(trim($food)) >= 2;
     }
-    // All choices are valid
-    return true;
+
+    //Return true if meal is valid
+    static function validMeal($meal)
+    {
+        return in_array($meal, DataLayer::getMeals());
+    }
+
+    //Return true if *all* condiments are valid
+    static function validCondiments($condiments)
+    {
+        $validCondiments = DataLayer::getCondiments();
+
+        //Make sure each selected condiment is valid
+        foreach ($condiments as $userChoice) {
+            if (!in_array($userChoice, $validCondiments)) {
+                return false;
+            }
+        }
+
+        //All choices are valid
+        return true;
+    }
 }
